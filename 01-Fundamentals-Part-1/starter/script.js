60646b8ae4b67b22f71a389ad3227af46454fd0d
Aman Kumar <amankumar005562@gmsil.com>
2025-02-22 20:15:14 +0530

    commit this config

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,38 @@
+let js = "amazing";
+// if (js == "amazing") alert("JavaScript is FUN!");
+
+// 40+8+23-10;
+console.log(40+8+23-10);
+
+console.log ('aman');
+console.log(32);
+
+let firstName = "abhishek";
+let first = "aman";
+let firstPerson = "kajal";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3;  Uncaught SyntaxError: Invalid or unexpected token
+
+// let aman&abhishek = 'JM';  Uncaught SyntaxError: Unexpected token '&'
+
+let aman_abhishek = "JM";
+
+// let function = 27;  script.js:24 Uncaught SyntaxError: Unexpected token 'function'
+
+let $function = 27;
+let Person = 'aman';
+let PI = 3.1415;
+let myFirstJob = "Coder";
+let myCurrentJob = "Software Engineer";
+
+let job1 = "programmer";
+let job2 = "software engineer";
+
+console.log(myFirstJob);
+
+
+
