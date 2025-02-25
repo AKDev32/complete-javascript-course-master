129e2d45cc06aa4b409b3f28852a6c8f42aa6df1
Aman Kumar <amankumar005562@gmsil.com>
2025-02-25 22:11:57 +0530

    commit config

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -58,6 +58,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 let age = 20;
 age = 21;
 
@@ -70,6 +71,138 @@ job = 'teacher';
 
 lastName = 'Kumar';
 console.log(lastName);
+*/
+
+/*
+const now = 2045
+const ageAman = now - 2025;
+const ageSarah = now - 2028;
+console.log(ageAman, ageSarah);
+console.log(ageAman * 2, ageAman / 10, 2**3);
+// 2**3 means 2 to the power of 3 
+
+const firstName = 'aman';
+const lastName = 'kumar';
+console.log(firstName +' ' + lastName);
+
+// Assignment operator
+let x = 10 + 5;  // 15
+x += 10;         // x = x + 10
+x *= 4;          // 100
+x++;             // x + 1
+x--;             // x - 1
+x--;             // x - 1
+console.log(x);
+
+// Comparison operator
+console.log(ageAman > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+*/
+
+/*
+const now = 2045
+const ageAman = now - 2025;
+const ageSarah = now - 2028;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageAman + ageSarah) / 2;
+console.log(ageAman, ageSarah, averageAge);
+*/
+
+// Coding Challenge 1
+/*
+// const AmanMass = 78;
+// const AmanHeight = 1.69;
+// const AbhishekMass = 95;
+// const AbhishekHeight = 1.88;
+
+const AmanMass = 55;
+const AmanHeight = 1.71;
+const AbhishekMass = 50;
+const AbhishekHeight = 1.68;
+
+const BMIAman = AmanMass / AmanHeight ** 2;
+const BMIAbhishek = AbhishekMass / (AbhishekHeight * AbhishekHeight);
+const markHeightBMI = BMIAman > BMIAbhishek;
+console.log(BMIAman, BMIAbhishek, markHeightBMI);
+*/
+
+/*
+const firstName = 'Aman';
+const job = 'Programmer';
+const birthYear = 2004;
+const year = 2025;
+
+const aman = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log (aman); 
+
+const amanNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(amanNew);
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+    multiple \n\
+    lines');
+
+console.log(`String 
+    multiple 
+    lines`);
+*/
+
+/*
+const age = 9;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving license 🚗🚗🚗`);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years`); 
+}
+
+const birthYear = 2004;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
+
+/*
+// Coding challenge 2
+
+const AmanMass = 50;
+const AmanHeight = 1.71;
+const AbhishekMass = 55;
+const AbhishekHeight = 1.68;
+
+const BMIAman = AmanMass / AmanHeight ** 2;
+const BMIAbhishek = AbhishekMass / (AbhishekHeight * AbhishekHeight);
+const markHeightBMI = BMIAman > BMIAbhishek;
+console.log(BMIAman, BMIAbhishek, markHeightBMI);
+if (BMIAman > BMIAbhishek) {
+    console.log(`Aman BMI is greater than the Abhishek BMI and value of Aman's BMI : ${BMIAman}`);
+} else {
+    console.log(`Abhishek BMI is greater than the Aman BMI and value of Abhishek's BMI : ${BMIAbhishek}`);
+}
+*/
+     
+
+
+
+
+
+
+
+
 
 
 
